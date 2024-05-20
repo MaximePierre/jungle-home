9eb65b9851c2f37c17601f7aca733585d3253aa9
PIERRE Maxime <Maximep30@hotmail.fr>
2024-05-20 10:04:23 +0200

    mod  ShoppingList

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,16 +1,75 @@
 import {plantList} from '../datasTest/plantList'
+// import {useState} from 'react';
 
+
+const categorys = [];
+
+
+    plantList.forEach(function (object) {
+    
+        let categoryIsUnik = categorys.includes(object.category);
+        
+        if ( categoryIsUnik === false) {
+            categorys.push(object.category);
+        }else {
+            // send to error page
+        }       
+    });
+//console.log(categorys);
+
+
+   
+
+
+  
 export default function ShoppingList() {
+
+    function CategorySelected(value){
+    const productOnCat = [];
+    //console.log(productOnCat)
+    
+     const keys = value.target.value;
+    
+    plantList.forEach(function (object){
+        let objectIsUnik = productOnCat.includes(keys)
+        if (object.category === keys){
+            if (objectIsUnik === false){
+                productOnCat.push(object)
+            }
+             
+         }
+     })
+     console.log(productOnCat)
+     return (   
+                productOnCat.map(product =>
+                    <li key={product}>
+                        <p>{product.name}</p>
+                    </li>
+        )
+     )
+  }
     return (
         <>
             <ul>
-                {plantList.map(plant => 
+                {
+                   categorys.map(cat => 
+                                        <li key={cat}>
+                                           <button value={cat} onClick={CategorySelected}>
+                                                {cat}
+                                           </button> 
+                                        </li>
+                                        )
+                }
+                {/* {plantList.map(plant => 
                                         <li key={plant.id}>
                                             <p>
-                                                {plant.name}
+                                                {plant.category}
                                             </p>
                                         </li>
-                )}
+                )} */}
+            </ul>
+            <ul>
+                 {/* <CategorySelected /> */}
             </ul>
         </>
 
